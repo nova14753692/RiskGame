370fb62bb761ad4a19dfd5a95762495992d57791
Rafael Sanchez <42717360+rasanchez180@users.noreply.github.com>
2018-09-20 21:19:14 -0500

    My portion

---
+++ src/main/Calvary.java
@@ -0,0 +1,11 @@
+
+public class Calvary extends Army {
+
+    public Calvary(int numbOfArmy) {
+        super(numbOfArmy);
+        armyName = "Calvary";
+        lowerBound = 3;
+        upperBound = 5;
+        nextType = "";
+    }
+}
