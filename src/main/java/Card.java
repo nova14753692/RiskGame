e63712c765f7ba934142ca7684850e3f552e570b
Ton <tontran147@outlook.com>
2018-09-22 23:19:08 -0500

    Recheck

---
+++ src/main/java/Card.java
@@ -0,0 +1,12 @@
+public abstract class Card {
+
+    protected String cardName;
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+}
